62df136cc2c31f234768a1123801db9086989d7b
farhan <farhandiwan0805@gmail.com>
2023-04-25 21:50:12 +0700

    ubah tema

--- page/debconfig/index.js
+++ page/debconfig/index.js
@@ -2,15 +2,15 @@ console.log("tes")
 
 let nav = document.querySelector('nav');
 
-window.addEventListener('scroll', function () {
-    if (window.pageYOffset > 100) {
-        nav.classList.add('shadow');
-        nav.style.backgroundColor = '#0d6efd'
-    } else {
-        nav.classList.remove('shadow');
-        nav.style.backgroundColor = null
-    }
-});
+// window.addEventListener('scroll', function () {
+//     if (window.pageYOffset > 100) {
+//         nav.classList.add('shadow');
+//         nav.style.backgroundColor = '#0d6efd'
+//     } else {
+//         nav.classList.remove('shadow');
+//         nav.style.backgroundColor = null
+//     }
+// });
 
 function onClick(element) {
     document.getElementById("img01").src = element.src;
